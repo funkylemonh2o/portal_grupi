a5d886384f551eda4f8531fe722df1b5149f44ad
LucyakDima <lucakdy483@gmail.com>
2025-07-04 20:27:57 +0300

    added CustomUser  model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLES = (
+        ('user', 'Користувач'),
+        ('moderator', 'Модератор'),
+        ('admin', 'Адміністратор'),
+    )
+    role = models.CharField(max_length=20, choices=ROLES, default='user')
+

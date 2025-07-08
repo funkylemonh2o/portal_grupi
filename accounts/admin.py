abb06fc8535a77f84dc3137c95eaa842222bda17
LucyakDima <lucakdy483@gmail.com>
2025-07-08 14:05:49 +0300

    added roles for users

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Додаткова інформація', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Додаткова інформація', {'fields': ('role',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)

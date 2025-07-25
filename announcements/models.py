e25afc97f21725d89a68cd27021a898c9d24a7a8
LucyakDima <lucakdy483@gmail.com>
2025-07-25 19:02:58 +0300

    fixed branch_vova

--- announcements/models.py
+++ announcements/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class Announcement(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def str(self):

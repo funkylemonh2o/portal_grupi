d4663deb041dab9ac82be90991aec5bd1f731e6e
LucyakDima <lucakdy483@gmail.com>
2025-08-05 14:04:01 +0300

    added styles for polls

--- event_calendar/urls.py
+++ event_calendar/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('calendar/', views.event_calendar_view, name='event_calendar')
+    path('calendar/', views.event_calendar_view, name='event_calendar'),
+    path('calendar/delete/<int:pk>/', views.event_delete, name='event_delete'),
 ]
--- event_calendar/views.py
+++ event_calendar/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Event
-# Create your views here.
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.views.decorators.http import require_POST
+
+def is_staff_user(user):
+    return user.is_staff or user.is_superuser
+
 def event_calendar_view(request):
     events = Event.objects.all().order_by('date')
     return render(request, 'events_calendar.html', {'events': events})
+
+@login_required
+@user_passes_test(is_staff_user)
+@require_POST
+def event_delete(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    event.delete()
+    return redirect('event_calendar')
